3349b88d24f2b90a2b0872ff95095a60bda2c01f
exigent-dev5 <rbossman@exigent-group.com>
2024-11-07 13:03:31 +0200

    Updated modal

--- spectrum/resources/js/components/therapists/TherapistsTable.vue
+++ spectrum/resources/js/components/therapists/TherapistsTable.vue
@@ -174,7 +174,7 @@
   </div>
    <!-- Add therapist Modal -->
    <div v-if="isAddtherapistModalOpen" class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div class="bg-white p-8 rounded-lg shadow-lg w-1/3">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md sm:w-2/3 md:w-1/2 lg:w-1/3 max-h-screen overflow-y-auto">
           <h2 class="text-2xl font-bold mb-4">Add therapist</h2>
           <form @submit.prevent="submitAddTherapist">
             <div class="mb-4">
--- spectrum/resources/js/components/users/UsersTable.vue
+++ spectrum/resources/js/components/users/UsersTable.vue
@@ -172,7 +172,7 @@
   </div>
    <!-- Add User Modal -->
    <div v-if="isAddUserModalOpen" class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-        <div class="bg-white p-8 rounded-lg shadow-lg w-1/3">
+    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md sm:w-2/3 md:w-1/2 lg:w-1/3 max-h-screen overflow-y-auto">
           <h2 class="text-2xl font-bold mb-4">Add User</h2>
           <form @submit.prevent="submitAddUser">
             <div class="mb-4">
