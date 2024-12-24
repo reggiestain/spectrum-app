7f96f31b1625f0404ace67123a42b07d02c9c70b
exigent-dev5 <rbossman@exigent-group.com>
2024-12-24 15:21:47 +0200

    Updated teacher paginate page number to 50

--- spectrum/resources/js/components/students/StudentsTable.vue
+++ spectrum/resources/js/components/students/StudentsTable.vue
@@ -78,7 +78,7 @@
             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Therapist(s)</th>
             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Teacher(s)</th>
             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Created</th>
-            <th class="px-6 py-3">Actions</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Actions</th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
