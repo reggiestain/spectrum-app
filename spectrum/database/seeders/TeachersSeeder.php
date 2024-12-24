1a6f145b2e1ea04d3577ba17ee1442817abde3aa
exigent-dev5 <rbossman@exigent-group.com>
2024-12-24 14:41:29 +0200

    Updated teacher seeder

--- spectrum/database/seeders/TeachersSeeder.php
+++ spectrum/database/seeders/TeachersSeeder.php
@@ -15,21 +15,12 @@ class TeachersSeeder extends Seeder
     public function run(): void
     {
         $teachers = [
-            ['first_name' => 'Angela', 'last_name' => 'Asumah','email'=>'Angela@gmail.com'],
-            ['first_name' => 'Shilla', 'last_name' => 'Klu','email'=>'test2@gmail.com'],
-            ['first_name' => 'Amy', 'last_name' => 'Awortwe','email'=>'test3@gmail.com'],
-            ['first_name' => 'Gideon', 'last_name' => 'Nii Sai Obodai','email'=>'test4@gmail.com'],
-            ['first_name' => 'Jennifer', 'last_name' => 'Yenimi Grant','email'=>'test5@gmail.com'],
-            ['first_name' => 'Richmond', 'last_name' => 'Doe Amedze','email'=>'test6@gmail.com'],
-            ['first_name' => 'Paula', 'last_name' => 'Owusu','email'=>'test7@gmail.com'],
-            ['first_name' => 'Monica', 'last_name' => 'Attiogbe','email'=>'test8@gmail.com'],
-            ['first_name' => 'Rita', 'last_name' => 'Ejodnawo','email'=>'test9@gmail.com'],
-            ['first_name' => 'Mariam', 'last_name' => 'Nasiru','email'=>'test10@gmail.com'],
-            ['first_name' => 'Doris', 'last_name' => 'Kobeh','email'=>'test12@gmail.com'],
-            ['first_name' => 'Joyce', 'last_name' => 'Simpiri','email'=>'test28@gmail.com'],
-            ['first_name' => 'Linda', 'last_name' => 'Nugah','email'=>'test23@gmail.com'],
-            ['first_name' => 'Patience', 'last_name' => 'Koranteng','email'=>'test34@gmail.com'],
-            ['first_name' => 'Stephanie', 'last_name' => '','email'=>'test35@gmail.com'], // No last name provided for Stephanie
+            ['first_name' => 'Angela', 'last_name' => 'Asumah', 'email' => 'Angela@gmail.com'],
+            ['first_name' => 'Stephanie', 'last_name' => 'Mensah', 'email' => 'stephanie.mensah@gmail.com'],
+            ['first_name' => 'Patience', 'last_name' => 'Koranteng', 'email' => 'patience.koranteng@gmail.com'],
+            ['first_name' => 'Amy', 'last_name' => 'Awortwe', 'email' => 'amy.awortwe@gmail.com'],
+            ['first_name' => 'Lydia', 'last_name' => '', 'email' => 'lydia@gmail.com'], // Empty last name if applicable
+            ['first_name' => 'Doris', 'last_name' => 'Doe Amedze', 'email' => 'doris.doe.amedze@gmail.com'],
         ];
 
         foreach ($teachers as $teacher) {
